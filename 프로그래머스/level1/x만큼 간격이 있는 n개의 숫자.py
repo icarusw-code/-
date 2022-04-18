33f7ee8dc72f9e335d8732bdbc85ba87d71c847d
icrausw <cordelia357@naver.com>
2022-04-18 22:08:41 +0900

    update

---
+++ 프로그래머스/level1/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,6 @@
+def solution(x, n):
+    answer = [0] * n
+    for i in range(n):
+        answer[i] = x * (i+1) 
+        
+    return answer
---
+++ 프로그래머스/level1/문자열 내 p와 y의 개수.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = True
+    s = s.lower()
+    
+    if s.count('p') == s.count('y'):
+        return True
+    elif s.count('p')==0 and s.count('y')==0:
+        return True
+    else:
+        return False
